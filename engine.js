a9e55479d506b9534197e79c6fcdec1cd08945cc
clannzu2 <clanzu2@yahoo.com>
2020-02-15 17:46:23 +0000

    first commit

---
+++ engine.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var button = $('#click');
+    
+
+    $( "#click" ).click(function() {
+alert( "you clicked me" );
+button.css('background-color','red');
+
+});
+})
